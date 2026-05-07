4921e3c1a57028ce1e8be11caa6431d4058296af
TGWL <tgwl@app.com>
2026-05-07 17:51:56 +0200

    bump: SW v67

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* ═══════════════════════════════════════════════
    TGWL Service Worker — PWA offline support
 ═══════════════════════════════════════════════ */
-const CACHE_NAME = 'tgwl-v1.95';
-const STATIC_CACHE = 'tgwl-static-v66.0';
-const DYNAMIC_CACHE = 'tgwl-dynamic-v66.0';
+const CACHE_NAME = 'tgwl-v1.96';
+const STATIC_CACHE = 'tgwl-static-v67.0';
+const DYNAMIC_CACHE = 'tgwl-dynamic-v67.0';
 
 const STATIC_ASSETS = [
   '/',
